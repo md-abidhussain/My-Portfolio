f9087bbf0a8b5829c3b1a1e2554794f68acace63
Mohd Abid Hussain <148795980+md-abidhussain@users.noreply.github.com>
2024-11-03 23:13:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you! Your message has been sent.");
+    this.reset();
+});
